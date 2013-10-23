5e953bc299f150c1c4a82e13d9fb00a217024d7a
MightyBear007@gmail.com <mightybear007@gmail.com>
2013-10-23 16:29:53 +0200

    custom >> omni

--- omni_honami.mk
+++ omni_honami.mk
@@ -16,7 +16,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Common Stuff
-$(call inherit-product, vendor/custom/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
